a778ec5acda426b404acbbe092358f5bd4071478
j-dover <aldover27@gmail.com>
2020-03-21 16:42:01 -0400

    changed h1 to all caps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Stardew Valley Quote Machine</h1>
+        <h1>STARDEW VALLEY QUOTE MACHINE</h1>
       </header>
       <QuoteBox/>
     </div>
